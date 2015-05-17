3cd2ca90818b415dc64793df266783531bd4d45c
austy81 <hausterlitz@gmail.com>
2015-05-17 10:14:50 +0200

    Modal

---
+++ AngularDemo/views/sport/sportEditController.js
@@ -0,0 +1,28 @@
+﻿app.controller('sportEditController', ['$scope', '$http', '$modalInstance', 'sport', function ($scope, $http, $modalInstance, sport) {
+
+    var apiUrl = 'http://localhost:56513/api';
+    var apiController = '/Sport/';
+
+    $scope.sport = sport;
+
+    $scope.ok = function () {
+        $scope.upadateSport();
+    };
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+
+    $scope.upadateSport = function () {
+        var data = $scope.sport;
+
+        $http.post(apiUrl + apiController, data)
+            .success(function (data, status, headers, config) {
+                $modalInstance.close();
+            })
+            .error(function (data, status, headers, config) {
+                alert(data.message);
+            });
+    };
+
+}]);
--- AngularDemo/views/sport/sportListController.js
+++ AngularDemo/views/sport/sportListController.js
@@ -1,37 +1,47 @@
-﻿app.controller('sportListController', ['$scope', '$http', function ($scope, $http) {
+﻿app.controller('sportListController', ['$scope', '$http', '$modal', function ($scope, $http, $modal) {
 
     var apiUrl = 'http://localhost:56513/api';
     var apiController = '/Sport/';
 
     $scope.isCreatingNewMeeting = false;
     $scope.sportsList = [];
+    $scope.sport = { Caption: 'exooooot' };
 
     var getSports = function () {
         $http.get(apiUrl + apiController)
-            .success(function(data) {
+            .success(function (data) {
                 $scope.sportsList = data;
             });
     }
 
     $scope.deleteSport = function (id) {
-        $http.delete(apiUrl + apiController + id).success(function() {
+        $http.delete(apiUrl + apiController + id).success(function () {
             getSports();
         });
-    }
+    };
 
-    $scope.createNewSport = function () {
-        var data = { caption: $scope.newSport };
-        $scope.newSport = '';
-
-        $http.post(apiUrl + apiController, data)
-            .success(function(data, status, headers, config) {
-                getSports();
-            })
-            .error(function(data, status, headers, config) {
-                
-            });
-    }
 
     getSports();
-    
+
+    $scope.editSport = function () {
+
+        var modalInstance = $modal.open({
+            animation: true,
+            templateUrl: 'sportEdit.html',
+            controller: 'sportEditController',
+            //size: size,
+            resolve: {
+                sport: function () {
+                    return $scope.sport;
+                }
+            }
+        });
+
+        modalInstance.result.then(function (sport) {
+            getSports();
+        }, function () {
+            //dismiss
+        });
+    };
+
 }]);
